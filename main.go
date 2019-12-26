d42f538a2fc7dbb6643c7306c862dc07e8658883
miguelramirez93 <darckzios@gmail.com>
2019-12-26 13:16:20 -0500

    Added custom libs

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
 	_ "github.com/udistrital/cuentas_contables_crud/routers"
 
 	"github.com/astaxie/beego"
+	"github.com/udistrital/utils_oas/customerror"
 	dbConnManager "github.com/udistrital/cuentas_contables_crud/db"
+	apistatus "github.com/udistrital/utils_oas/apiStatusLib"
 )
 
 func main() {
@@ -20,5 +22,10 @@ func main() {
 		beego.BConfig.WebConfig.DirectoryIndex = true
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
 	}
+
+	// Custom libs
+	beego.ErrorController(&customerror.CustomErrorController{})
+	apistatus.Init()
+
 	beego.Run()
 }
