17f275bec847b6d7f42e0c26d76eec33ec5c1faf
miguelramirez93 <darckzios@gmail.com>
2020-01-15 14:08:32 -0500

    Changed JSON responses fields on some services

--- models/detalle_cuenta_contable.go
+++ models/detalle_cuenta_contable.go
@@ -3,6 +3,6 @@ package models
 var DetalleCuentaContableCollection = "detalle_cuenta_contable"
 
 type DetalleCuentaContable struct {
-	ID    string `json:"ID" bson:"_id,omitempty"`
+	ID    string `json:"Id" bson:"_id,omitempty"`
 	Label string `json:"Label" bson:"label"`
 }
--- models/naturaleza_cuenta_contable.go
+++ models/naturaleza_cuenta_contable.go
@@ -4,6 +4,6 @@ var NaturalezaCuentaContableCollection = "naturaleza_cuenta_contable"
 
 // NaturalezaCuentaContable cuenta contable parameter.
 type NaturalezaCuentaContable struct {
-	ID    string `json:"ID" bson:"_id,omitempty"`
+	ID    string `json:"Id" bson:"_id,omitempty"`
 	Label string `json:"Label" bson:"label"`
 }
